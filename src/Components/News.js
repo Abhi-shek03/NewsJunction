d3ee91608f7638c4d6a37079aea260bc43dc1987
unknown <you@example.com>
2023-07-09 16:12:40 +0530

    fetching

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,22 +1,51 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import PropTypes from 'prop-types'
 
 export class News extends Component {
-
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general',
+  }
   
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,  
+  }
 
-  constructor(){
-    super();
+  capatliseFisrtLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  constructor(props){
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1,
     }
+    document.title = `NewsJunction | ${this.capatliseFisrtLetter(this.props.category)}`
+  }
+
+  async updateNews(){
+    try{        
+      const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a5ede75922e74b088a04ab02248fe46e&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        const res = await fetch(url);
+        const data = await res.json();
+        this.setState({
+            articles: data.articles, totalResults: data.totalResults
+        });
+    }
+    catch(e) {
+        console.log("something is not working");
+    }
   }
 
   async componentDidMount(){
     try{        
-      let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=a5ede75922e74b088a04ab02248fe46e&page=1";
+      let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a5ede75922e74b088a04ab02248fe46e&page=1&pageSize=${this.props.pageSize}`;
         const res = await fetch(url);
         const data = await res.json();
         this.setState({
@@ -28,56 +57,30 @@ export class News extends Component {
     }
 }
 
-  // async componentDidMount(){
-  //   let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=a5ede75922e74b088a04ab02248fe46e";
-  //   let data = await fetch(url);
-  //   let parsedData = await data.json()
-  //   this.setState = ({articles: parsedData.articles})
-  //   console.log(parsedData)
-
-  // }
-
       HandlePrevvClick = async() =>{
-
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a5ede75922e74b088a04ab02248fe46e&page=${this.state.page - 1}&pagrSize=20`;
-        const res = await fetch(url);
-        const data = await res.json();
-        this.setState({
-            articles: data.articles,
-            page: this.state.page - 1,
-        });
-        }
-
-      HandleNexxtClick = async () =>{
-      if (this.state.page + 1 > Math.ceil(this.state.totalResults/20)){
-
+        this.setState({page: this.state.page - 1})
+        this.updateNews()
       }
 
-      else {
-        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=a5ede75922e74b088a04ab02248fe46e&page=${this.state.page + 1}&pageSize=20`;
-        const res = await fetch(url);
-        const data = await res.json();
-        this.setState({
-            articles: data.articles,
-            page: this.state.page + 1,
-        });
-        }
+      HandleNexxtClick = async () =>{
+        this.setState({page: this.state.page + 1})
+        this.updateNews()
       }
 
   render() {
     return (
       <div className='container my-3'>
-        <h1>NewsJunction | TOP Headlines</h1>
+        <h1 className='text-center' style={{margin: '35px 0px'}}>NewsJunction | TOP {this.capatliseFisrtLetter(this.props.category)} Headlines</h1>
         <div className="row">
         {this.state.articles.map((element)=>{
-            return <div className="col-md-3" key={element.url}>
-              <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+            return <div className="col-md-4" key={element.url}>
+              <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
             </div>
         })}
         </div>
         <div className="container d-flex justify-content-between">
         <button type='button' disabled={this.state.page<=1} className='btn btn-dark' onClick={this.HandlePrevvClick}>&larr; Previous</button>
-        <button type='button' disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/20)}className='btn btn-dark' onClick={this.HandleNexxtClick}>Next &rarr;</button>
+        <button type='button' disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)}className='btn btn-dark' onClick={this.HandleNexxtClick}>Next &rarr;</button>
         </div>
       </div>
     )
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
         <div className="my-3">
-          <div className="card" style={{ width: "18rem" }}>
+          <div className="card">
+          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%', zIndex:'1'}}>{source}</span>
             <img src={imageUrl?imageUrl:'https://etimg.etb2bimg.com/photo/101562532.cms'} className="card-img-top" alt="..." />
             <div className="card-body">
-              <h5 className="card-title">{title}</h5>
+              <h5 className="card-title">{title} </h5>
               <p className="card-text">{description}</p>
+              <p className="card-text"><small className="text-body-secondary">By {author?author:'unknown'} on {new Date(date).toGMTString()}</small></p>
               <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-primary">
                 Read More
               </a>
